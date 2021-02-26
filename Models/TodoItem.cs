2f23fabe99e5b913361b9d75258b65aad292e0b9
DavidMBruening <76656332+DavidMBruening@users.noreply.github.com>
2021-02-26 10:24:11 -0500

    Add files via upload

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ASPCore2021Feb.Models {
+    public class TodoItem {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+        public DateTime dueDate { get; set; }
+    }
+}
